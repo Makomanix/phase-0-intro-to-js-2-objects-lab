4e07bf0aa3866209b6d29b075342dd2b7c89b5d3
Jonathan Bruckman <jbruckman86@gmail.com>
2022-09-24 14:44:46 -0400

    commit tree c62649fea211df4ba1549e3e6ffe4819f528cd16

--- index.js
+++ index.js
@@ -1,1 +1,112 @@
-// Write your solution in this file!
+// const employee = {
+//     name: 'Jon',
+//     streetAddress: 'My Street',
+// }
+
+// function updateEmployeeWithKeyAndValue(obj, key, value) {
+//     const newObj = {...obj};
+
+//     newObj[key] = value;
+
+//     return newObj;
+// }
+
+// const newEmployee = updateEmployeeWithKeyAndValue(
+//     employee,
+//     'streetAddress',
+//     'Not My Street'
+// );
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+//     obj[key] = value;
+
+//     return obj;
+// }
+
+// destructivelyUpdateEmployeeWithKeyAndValue(
+//     employee,
+//     'streetAddress',
+//     'Not My Street'
+// )
+
+// function deleteFromEmployeeByKey(obj, key, value) {
+//     const newerObj = {...obj}
+
+//     newerObj[key] = value
+
+//     return newerObj;
+// }
+
+// const newerEmployee = deleteFromEmployeeByKey(
+//     employee,
+//     'streetAddress',
+//     'Not My Street'
+// )
+
+// delete newerEmployee.streetAddress
+
+// function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+//     obj[key] = value;
+
+//     return obj;
+// }
+
+// const newestEmployee = destructivelyDeleteFromEmployeeByKey(
+//     employee,
+//     'streetAddress',
+//     'Not My Street'
+// )
+
+// delete newestEmployee.key
+
+const employee = {
+    name: 'Jon',
+    streetAddress: 'My Street'
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj}
+
+    newObj[key] = value
+
+    return newObj
+}
+
+// const newEmployee = updateEmployeeWithKeyAndValue(
+//     employee,
+//     'name',
+//     'Sam'
+// )
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+
+    return obj;
+}
+
+// const deadEmployee = destructivelyUpdateEmployeeWithKeyAndValue (
+//     employee,
+//     'name',
+//     'Sam'
+// )
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    const newerObJ = {...obj};
+
+    newerObJ[key] = value
+
+    return newerObJ;
+}
+
+// const newerEmployee = deleteFromEmployeeByKey(
+//     employee,
+//     'name',
+//     'Sam'
+// )
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value
+
+    return obj
+}
+
